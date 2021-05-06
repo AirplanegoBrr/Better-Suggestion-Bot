08df9f97c4b719504b40660d958ed44bc97cf9a1
AirplaneGoBrr <mottlcompound@gmail.com>
2021-05-06 01:04:25 -0700

    yes.

---
+++ commands/help.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'help',
+    description: 'help command.',
+    execute(message) {
+        message.channel.send("My **prefix** is ``s!``\nThis bot is VERY easy to use!\n\nDo ``s!suggest`` to suggest an idea!\n\nAlso ``s!ping`` is a command and its a *little* broken \n\n*and for the admins*\nDo ``s!channel`` to set the suggestion channel!\n\nand if you wanna use this bot use this link: ||<https://discord.com/oauth2/authorize?client_id=839235993789661215&permissions=68688&scope=bot> there will be a way SOON to disable this AD||")
+    }
+}
